59d8ef18ac093b242dfd010f63a15a271c9f9e38
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-05 10:54:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var length,breadth;
+length=4.5;
+breadth=6;
+area=1/2*length*breadth;
+
+function setup() {
+  createCanvas(400, 400);
+  
+
+
+  }
+
+function draw() {
+  background("yellow");
+  
+  console.log("Area of triangle"area);
+  drawSprites();
+}
